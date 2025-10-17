5f0b329ff0be0f8462e5d5e66c910b3f1dac8b1a
zazyzaya <zazyzaya@gmail.com>
2025-10-17 12:19:43 -0400

    Fixing bash errors

--- train_size_ablation_lanl2.sh
+++ train_size_ablation_lanl2.sh
@@ -5,6 +5,6 @@ declare -a pcts=("0.9" "0.05" "0.01")
 for i in "${pcts[@]}"
 do
    mkdir pretrained/rw_sampling/lanl14argus/"$i"_tr
-   python rw_pretrain.py --argus --device $1 --tr-size $i --log-out pretrained/rw_sampling/lanl14argus/$i_tr
+   python rw_pretrain.py --argus --device $1 --tr-size $i --log-out pretrained/rw_sampling/lanl14argus/"$i"_tr
    python snapshot_finetune.py --argus --device $1 --tr-size $i --walk-len 1 --model-fname pretrained/rw_sampling/lanl14argus/"$i"_tr/rw_bert_lanl14argus_tiny.pt --static
 done
